6dbfff8847283bb4434911bfc7f726cba53f4d84
tatar008 <jirapat_ja66@live.rmutl.ac.th>
2023-07-27 15:25:20 +0700

    lab5

--- Lab/Lab05-If-Else-Switch-Case.cpp
+++ Lab/Lab05-If-Else-Switch-Case.cpp
@@ -37,24 +37,26 @@
 
 int main() {
     int score = 0  ;
-    printf( "Enter your score : " ) ;
+    printf( "Enter your score : \n" ) ;
     scanf( "%d", &score ) ;
     if(score >=80 ) {
-        printf("A") ;
+        printf("A !") ;
     } else if( score >= 75 && score < 80 ) {
-        printf( "B+" ) ;
+        printf( "B+ !" ) ;
     } else if( score >= 70 && score < 75 ) {
-        printf( "B" ) ;
+        printf( "B !" ) ;
     } else if( score >= 65 && score < 70 ) {
-        printf( "C+") ;
+        printf( "C+ !") ;
     } else if( score >= 60 && score < 65 ) {
-        printf( "C" ) ;
+        printf( "C !" ) ;
     } else if( score >= 55 && score < 60 ) {
-        printf( "D+" ) ;
+        printf( "D+ !" ) ;
     } else if( score >= 50 && score < 55 ) {
-        printf( "D" ) ;
+        printf( "D !" ) ;
+    } else if ( !score ) {
+        printf( "please enter number only." ) ;
     } else {
-        printf( "F" ) ;
+        printf( "F !" ) ;
     }
     return 0 ;
 }//end main function
